ff947ee03517ab4724cf4afd612807c32c16b7ae
Andrew Plummer <plummer574@gmail.com>
2015-08-15 23:03:01 +0100

    Bump to 0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-migration-testcase',
-    version='0.0.2',
+    version='0.0.3',
     author='Andrew Plummer',
     author_email='plummer574@gmail.com',
     url='https://github.com/plumdog/django_migration_testcase',
