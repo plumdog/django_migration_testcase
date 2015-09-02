9f27f03beb222cd4c179e9de0d68da7260e22709
Andrew Plummer <plummer574@gmail.com>
2015-09-02 09:22:30 +0100

    Merge test apps

--- run_tests.sh
+++ run_tests.sh
@@ -8,16 +8,14 @@ DJANGO_VERSION=$("$VIRTUAL_ENV"/bin/django-admin.py --version)
 # the django migration tests for 1.7 and above
 if [[ $DJANGO_VERSION == "1.4"* || $DJANGO_VERSION == "1.5"* || $DJANGO_VERSION == "1.6"* ]]; then
     NAME='test_project_*_south'
-    APPNAME='test_app_south'
 else
     NAME='test_project_*_django'
-    APPNAME='test_app'
 fi
 
 
 for f in $(find tests -type d -name "$NAME"); do
     echo "Tests for $f"
-    if ! (cd "$f" && ./manage.py test "$APPNAME") ; then
+    if ! (cd "$f" && ./manage.py test test_app) ; then
 	SUCCESS=1;
     fi
 done
